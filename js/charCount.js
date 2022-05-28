8b9fe9940c3fa75be9b18b62146e50d92d730c25
fravila08 <105952966+fravila08@users.noreply.github.com>
2022-05-28 15:08:32 -0700

    cleaned up the string

--- js/charCount.js
+++ js/charCount.js
@@ -1,3 +1,9 @@
-exports.charCount = function() {
-
+//exports.
+const charCount = function(x) {
+    string=x.replace(/\s/g, "")
+    console.log(string)
+    
 };
+    
+
+charCount("an apple a day will keep the doctor away")
