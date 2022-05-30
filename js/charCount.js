75e4f43469a24ac4ba9a65a4ca5c99ae842dd799
fravila08 <105952966+fravila08@users.noreply.github.com>
2022-05-29 21:17:10 -0700

    done with JS

--- js/charCount.js
+++ js/charCount.js
@@ -1,14 +1,11 @@
-// exports.
-const charCount = function(x) {
+exports.charCount = function(x) {
     string=x.replace(/\s/g, "")
     let splitStr=[...string]
     // console.log(splitStr)
     // console.log(string)
     myObject={}
     splitStr.forEach(function (x) {myObject[x] = ((myObject[x] || 0) + 1); });
-    return(myObject)    
+    return(myObject)
 };
 
     
-
-console.log(charCount("aaabbc"))
