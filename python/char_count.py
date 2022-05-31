3f35ee3eeb83841f6d9de025c188764288f51028
fravila08 <105952966+fravila08@users.noreply.github.com>
2022-05-30 22:30:11 -0700

    CharCount complete

--- python/char_count.py
+++ python/char_count.py
@@ -1,2 +1,20 @@
+from operator import countOf
+import turtle
+from collections import OrderedDict
+
 def char_count(str):
-  # Your code here
+      str=str.replace(" ", "")
+      trueStr={} 
+      for i in str:
+        count=countOf(str, i)
+        key=i
+        #print(countOf(trueStr, key))
+        if countOf(trueStr, key)< 1:
+          obj={key:count}
+          trueStr.update(obj)
+      return trueStr
+      #trueStr=OrderedDict(sorted(trueStr.items()))
+      #print(trueStr)
+        
+      
+print(char_count("aaabbc"))
